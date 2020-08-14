874da11a4589755275f0e2becadb5b410dd26643
Ville Lehtonen <violehtonen@gmail.com>
2020-08-14 17:04:14 +0200

    quality control

--- data_preprocessing.R
+++ data_preprocessing.R
@@ -39,6 +39,31 @@ firstColumnToRowNames <- function(data) {
   return(data)
 }
 
+# Remove genes from a sce object that are expressed in less than x cells
+removeGeneExpLessThanX <- function(data, x) {
+  keep_feature <- rowSums(counts(data) > 0) >= x
+  data <- data[keep_feature, ]
+  return(data)
+}
+
+# Perform pel cell QC and detect mitochondrial transcripts
+mitoPerCellQC <- function(sce) {
+  location <- rowRanges(sce)
+  is.mito <- any(seqnames(location) == "MT")
+  sce <- addPerCellQC(sce, subsets = list(Mito = is.mito))
+  return(sce)
+}
+
+# Detect outliers (sum, detected, mito_percent)
+findOutliers <- function(sce) {
+  qc.lib.sce <- isOutlier(sce$sum, log = TRUE, type = "both", nmads = 4)
+  qc.nexprs.sce <- isOutlier(sce$detected, log = TRUE, type = "both", nmads = 4)
+  qc.mito.sce <- sce$subsets_Mito_percent > 20
+  
+  discard <- qc.lib.sce | qc.nexprs.sce | qc.mito.sce
+  return(discard)
+}
+
 #####################
 ### Load data ###
 #####################
@@ -107,10 +132,110 @@ dim(GSE143435_meta_d0)
 # GSE126834
 sce_GSE126834 <- SingleCellExperiment(assays = list(counts = GSE126834))
 
-# Save objects in RDS to avoid long computational times
+# Save objects to avoid long computational times
 saveRDS(sce_GSE143437, file = "sce_GSE143437")
 saveRDS(sce_GSE143435_d0, file = "sce_GSE143435_d0")
 saveRDS(sce_GSE143435_d2, file = "sce_GSE143435_d2")
 saveRDS(sce_GSE143435_d5, file = "sce_GSE143435_d5")
 saveRDS(sce_GSE143435_d7, file = "sce_GSE143435_d7")
 saveRDS(sce_GSE126834, file = "sce_GSE126834")
+
+#######################
+### Quality control ###
+#######################
+# Load files from a file (optional)
+#sce_GSE143437 <- readRDS("sce_GSE143437")
+#sce_GSE143435_d0 <- readRDS("sce_GSE143435_d0")
+#sce_GSE143435_d2 <- readRDS("sce_GSE143435_d2")
+#sce_GSE143435_d5 <- readRDS("sce_GSE143435_d5")
+#sce_GSE143435_d7 <- readRDS("sce_GSE143435_d7")
+#sce_GSE126834 <- readRDS("sce_GSE126834")
+
+# Remove genes that are expressed in less than 3 cells
+sce_GSE143437 <- removeGeneExpLessThanX(sce_GSE143437, 3)
+sce_GSE143435_d0 <- removeGeneExpLessThanX(sce_GSE143435_d0, 3)
+sce_GSE143435_d2 <- removeGeneExpLessThanX(sce_GSE143435_d2, 3)
+sce_GSE143435_d5 <- removeGeneExpLessThanX(sce_GSE143435_d5, 3)
+sce_GSE143435_d7 <- removeGeneExpLessThanX(sce_GSE143435_d7, 3)
+sce_GSE126834 <- removeGeneExpLessThanX(sce_GSE126834, 3)
+
+# Retrieve mitochondrial transcripts and perform per cell QC
+sce_GSE143437 <- mitoPerCellQC(sce_GSE143437)
+sce_GSE143435_d0 <- mitoPerCellQC(sce_GSE143435_d0)
+sce_GSE143435_d2 <- mitoPerCellQC(sce_GSE143435_d2)
+sce_GSE143435_d5 <- mitoPerCellQC(sce_GSE143435_d5)
+sce_GSE143435_d7 <- mitoPerCellQC(sce_GSE143435_d7)
+sce_GSE126834 <- mitoPerCellQC(sce_GSE126834)
+
+# Save objects
+saveRDS(sce_GSE143437, file = "sce_GSE143437_qc")
+saveRDS(sce_GSE143435_d0, file = "sce_GSE143435_d0_qc")
+saveRDS(sce_GSE143435_d2, file = "sce_GSE143435_d2_qc")
+saveRDS(sce_GSE143435_d5, file = "sce_GSE143435_d5_qc")
+saveRDS(sce_GSE143435_d7, file = "sce_GSE143435_d7_qc")
+saveRDS(sce_GSE126834, file = "sce_GSE126834_qc")
+
+# Identify outliers (low quality cells)
+discard_sce_GSE143437 <- findOutliers(sce_GSE143437)
+discard_sce_GSE143435_d0 <- findOutliers(sce_GSE143435_d0)
+discard_sce_GSE143435_d2 <- findOutliers(sce_GSE143435_d2)
+discard_sce_GSE143435_d5 <- findOutliers(sce_GSE143435_d5)
+discard_sce_GSE143435_d7 <- findOutliers(sce_GSE143435_d7)
+discard_sce_GSE126834 <- findOutliers(sce_GSE126834)
+
+sce_GSE143437$discard <- discard_sce_GSE143437
+sce_GSE143435_d0$discard <- discard_sce_GSE143435_d0
+sce_GSE143435_d2$discard <- discard_sce_GSE143435_d2
+sce_GSE143435_d5$discard <- discard_sce_GSE143435_d5
+sce_GSE143435_d7$discard <- discard_sce_GSE143435_d7
+sce_GSE126834$discard <- discard_sce_GSE126834
+
+# Discard outliers
+sce_GSE143437.f <- sce_GSE143437[, sce_GSE143437$discard == FALSE]
+sce_GSE143435_d0.f <- sce_GSE143435_d0[, sce_GSE143435_d0$discard == FALSE]
+sce_GSE143435_d2.f <- sce_GSE143435_d2[, sce_GSE143435_d2$discard == FALSE]
+sce_GSE143435_d5.f <- sce_GSE143435_d5[, sce_GSE143435_d5$discard == FALSE]
+sce_GSE143435_d7.f <- sce_GSE143435_d7[, sce_GSE143435_d7$discard == FALSE]
+sce_GSE126834.f <- sce_GSE126834[, sce_GSE126834$discard == FALSE]
+
+# Save objects
+saveRDS(sce_GSE143437.f, file = "sce_GSE143437_qc_f")
+saveRDS(sce_GSE143435_d0.f, file = "sce_GSE143435_d0_qc_f")
+saveRDS(sce_GSE143435_d2.f, file = "sce_GSE143435_d2_qc_f")
+saveRDS(sce_GSE143435_d5.f, file = "sce_GSE143435_d5_qc_f")
+saveRDS(sce_GSE143435_d7.f, file = "sce_GSE143435_d7_qc_f")
+saveRDS(sce_GSE126834.f, file = "sce_GSE126834_qc_f")
+
+#sce_GSE143437.f <- readRDS("sce_GSE143437_qc_f")
+#sce_GSE143435_d0.f <- readRDS("sce_GSE143435_d0_qc_f")
+#sce_GSE143435_d2.f <- readRDS("sce_GSE143435_d2_qc_f")
+#sce_GSE143435_d5.f <- readRDS("sce_GSE143435_d5_qc_f")
+#sce_GSE143435_d7.f <- readRDS("sce_GSE143435_d7_qc_f")
+#sce_GSE126834.f <- readRDS("sce_GSE126834_qc_f")
+
+#####################
+### Normalization ###
+#####################
+lib.sf <- librarySizeFactors(sce_GSE143437.f)
+set.seed(100)
+quickie <- quickCluster(sce_GSE143437.f)
+table(quickie)
+
+
+
+# Log normalization
+
+
+# Cell cycle scoring (https://satijalab.org/seurat/v3.2/cell_cycle_vignette.html)
+
+
+
+
+
+
+
+
+
+
+
+
